c453248d93d247ddfb62b29ffa1ccb48bc5c2576
N-HADDAM <naoufel.haddam2022@campus-eni.fr>
2022-09-14 16:28:31 +0200

    Commit branche 1040

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
-use App\Repository\CampusRepository;
+use App\Entity\Campus;
+use App\Entity\Sortie;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function PHPUnit\Framework\isEmpty;
 
 class MainController extends AbstractController
 {
+
+    public EntityManagerInterface $entityManager;
+    private Response $response;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'app_default', methods: ['GET'])]
     public function default(): Response {
         return $this->redirectToRoute('app_main');
@@ -21,20 +34,112 @@ class MainController extends AbstractController
         return $this->render('main/index.html.twig', []);
     }
 
-    #[Route('/main', name: 'app_main', methods: ['GET', 'POST'])]
+    #[Route('/main', name: 'app_main', methods: ['GET'])]
     public function list(
-        SortieRepository $sortieRepository,
-        CampusRepository $campusRepository,
+        Request $request,
+        SortieRepository $sortieRepository
     ): Response
     {
-        $campus = $campusRepository->findAll();
+        $campus = $this->getUser()->getCampus();
+        $listeSorties = $sortieRepository->findBy(['siteOrganisateur'=>$campus]);
 
-        $sorties =$sortieRepository->findAll();
+//        dd($request->query->get('nomSortieContient'));
+        if ($request->query->get('nomSortieContient') !== null){
+            $portion = $request->query->get('nomSortieContient');
+            dd($portion);
+        }
+
+//        if ($request->isMethod('get'))
+//        {
+
+//
+//
+//            return $this->showAllSorties();
+//        } else
+//        {
+//            if (!isEmpty($_POST['campus'])
+//
+//
+//            ) {
+//            $response = new Response($this->filtreCampus());
+//
+//            }
+//            if (
+//            isEmpty($_POST['campus'])
+//            && isset($_POST['dateMin'])
+//            && isset($_POST['dateMax'])
+//
+//            && !isset($_POST['organisateurTrue'])
+//            && !isset($_POST['inscritTrue'])
+//            && !isset($_POST['inscritFalse'])
+//            && !isset($_POST['sortiesPassees'])
+//            ) {
+//                    $response = new Response($this->dateNotNull());
+//            }
+//        return new Response($this->response = $response);
+//        }
 
         return $this->render('main/index.html.twig', [
-            "sorties" =>$sorties,
-            "campus" =>$campus
+            "sorties" => null,
+            "campus" => $this->getCampus()
         ]);
+    }
+
+// ---------------------------------------------------------------------------------------------
+
+    public function showAllSorties(): Response{
+        $campus = $this->getCampus();
+
+        $sorties = $this->entityManager
+            ->getRepository(Sortie::class)
+            ->findAll();
+
+        return $this->render('main/index.html.twig', [
+            "sorties" => $sorties,
+            "campus" => $campus,
+        ]);
+    }
+
+    public function dateNotNull(): Response{
+        $campus = $this->getCampus();
+
+        $dateMin = $_POST['dateMin'];
+        $dateMax = $_POST['dateMax'];
+
+        $sorties = $this->entityManager
+            ->getRepository(Sortie::class)
+            ->findDateFiltered($dateMin, $dateMax);
+
+        return $this->render('main/index.html.twig', [
+            "sorties" => $sorties,
+            "campus" => $campus,
+        ]);
+
+
+    }
+
+    public function filtreCampus(SortieRepository $sortieRepository): array{
+
+//        dd('filtre campus');
+
+        $campusChoisi = $_POST['campus'];
+        $campusSelectionne = $this->entityManager
+            ->getRepository(Campus::class)
+            ->findOneBy(['nom' => $campusChoisi]);
+        $campusId = $campusSelectionne->getId();
+
+        $sorties = $this->entityManager
+            ->getRepository(Sortie::class)
+            ->findBy(['siteOrganisateur' => $campusId]);
+        return $sorties;
+    }
+
+    public function getCampus(): array
+    {
+        $campus =  $this->entityManager
+            ->getRepository(Campus::class)
+            ->findAll();
+        return $campus;
     }
 
 }
