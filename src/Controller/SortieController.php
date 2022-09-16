c7f75590404fd82501103565c60b7a9a74d447fa
ThomasCornillet <thomas.cornillet@gmail.com>
2022-09-16 21:30:20 +0200

    commit qui ne veut rien dire mais j'ai oublié ce qu'a dit Yoann

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -219,6 +219,38 @@ class SortieController extends AbstractController
         ]);
     }
 
+    #[Route('/modifier/{i}', name: 'modifier', methods: ['GET', 'POST'])]
+    public function modifier(
+        int $i,
+        SortieRepository $sortieRepository,
+        VilleRepository $villeRepository,
+        Request $request,
+        CampusRepository $campusRepository,
+    ): Response
+    {
+        $sortie = $sortieRepository->find($i);
+        if ($this->getUser()->getUserIdentifier() !== $sortie->getOrganisateur()->getUserIdentifier()) {
+            $this->addFlash('danger', 'Vous ne pouvez pas modifier cette sortie !');
+            return $this->redirectToRoute('app_sortie_detail', ['i' => $i]);
+        }
+        $villes = $villeRepository->findAll(); // TODO revoir par rapport à ce que disait Philippe surles findAll
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // TODO traiter le formulaire
+            $sortie->setSiteOrganisateur($_POST['']);
+        }
+
+        $listCampus = $campusRepository->findAll();
+
+        return $this->renderForm('sortie/create.html.twig', [
+            'form' => $form,
+            'villes' => $villes,
+            'sortie' => $sortie,
+            'listeCampus' => $listCampus,
+        ]);
+
+    }
 
  // ----------------------------------------------------------------------------------------------------------------------
 
