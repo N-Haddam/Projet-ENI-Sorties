51166b56306e203dae393c211739006a050199d4
N-HADDAM <naoufel.haddam2022@campus-eni.fr>
2022-09-19 12:53:44 +0200

    Commit branche add easyAdmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(ParticipantCrudController::class)->generateUrl();
+        return $this->redirect($url);
+//        return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+//         return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Projet Sorties');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+            yield MenuItem::section('Utilisateurs'),
+            yield MenuItem::linkToCrud('Participants', 'fas fa-list', Participant::class),
+            yield MenuItem::section('Sorties'),
+            yield MenuItem::linkToCrud('Sorties', 'fas fa-list', Sortie::class)
+        ];
+    }
+}
